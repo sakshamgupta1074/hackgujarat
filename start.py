c5f7ef8fecddf605d0150e557a3c3d281d9c4277
Kritika Ahuja <54718939+kritikaahuja28@users.noreply.github.com>
2020-09-06 02:40:30 +0530

    Add files via upload

---
+++ start.py
@@ -0,0 +1,16 @@
+import json 
+from pymongo import MongoClient   
+
+myclient = MongoClient("mongodb://localhost:27017/")  
+
+db = myclient["patients"] 
+
+Collection = db["data"] 
+  
+with open('result.json') as file: 
+    file_data = json.load(file) 
+      
+if isinstance(file_data, list): 
+    Collection.insert_many(file_data)   
+else: 
+    Collection.insert_one(file_data) 
