5e345218c6aaa05e51c61e053b17477bedf36f65
shiva2410 <thavani.shiva3@gmail.com>
2020-09-06 15:59:48 +0530

    modified json format

--- jsontodict.py
+++ jsontodict.py
@@ -3,5 +3,5 @@ import json
 
 with open('responses.json') as f_in:
 	my_data = json.load(f_in)
-	print(my_data['fname'])
-	print(my_data['lname'])
+	print(my_data['userinfo']['fname'])
+	print(my_data['userinfo']['lname'])
